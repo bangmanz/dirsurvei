b8c610373944bc59a947c42ef0d4cc941f722493
bangmanz <arahman.assel@gmail.com>
2019-07-13 13:39:22 +0800

    membuat controller dan view daftar survei

--- application/controllers/Mahasiswa.php
+++
@@ -1,4 +0,0 @@
-<?php
-
-
-class Mahasiswa extends CI_Controller
---
+++ application/controllers/Survei.php
@@ -0,0 +1,13 @@
+<?php
+
+
+class Survei extends CI_Controller{
+public function index ()
+{
+  $this->load-view('templates/header')  ;
+  $this->load-view('mahasiswa/index')  ;
+  $this->load-view('templates/footer')  ;
+
+}
+
+}
