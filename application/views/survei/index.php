6e6ecbcac2183e3893a40da2c6af860e8c2ed11b
bangmanz <arahman.assel@gmail.com>
2019-07-29 16:46:26 +0800

    fix beres database

--- application/views/survei/index.php
+++ application/views/survei/index.php
@@ -4,12 +4,18 @@
     <div class="row-mt-3">
         <div class="col-md-6">
         <h1>Daftar Survei</h1>
-            <ul class="list-group">
-                <?php foreach ($survei as $srv);?>
-                    <li class="list-group-item"><?=$srv['nama']; ?></li>
-                
             
-            </ul>
+            <?php foreach ($survei as $srv):?>
+            <div class="card" style="width: 18rem;">
+                <div class="card-body">
+                <h5 class="card-title"><?=$srv['singkatan'] ;?></h5>
+                <h6 class="card-subtitle mb-2 text-muted"><?=$srv['tahun']; ?></h6>
+                <p class="card-text"><?=$srv['nama']; ?></p>
+                <a href="#" class="card-link">Detil.. </a>
+            </div>
+            <?php endforeach; ?>
+        
+        </div>
         </div>
 
     </div>
